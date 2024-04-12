613d8f02387eaecac4df3511f80cb64ca9e9af11
Rodolfo Bittkau <tyrmund@gmail.com>
2024-04-12 17:10:47 +0200

    done

--- src/calculateArea.js
+++ src/calculateArea.js
@@ -1,3 +1,8 @@
-function calculateArea() {
-
+function calculateArea(height, width) {
+    if (typeof (height) !== 'number' || typeof (width) !== 'number') {
+        return undefined;
+    }
+    else {
+        return height * width;
+    }
 }
--- tests/calculateArea.spec.js
+++ tests/calculateArea.spec.js
@@ -12,3 +12,36 @@ describe("Iteration 3", () => {
 })
 
 */
+
+describe("Iteration 3", () => {
+    describe("Function - calculateArea", () => {
+        it("should be defined", () => {
+            expect(calculateArea).toBeDefined();
+        });
+
+        it("should take two arguments", () => {
+            expect(calculateArea.length).toBe(2);
+        });
+
+        it("should return a number representing the area of a rectangle", () => {
+            expect(calculateArea(1, 2)).toEqual(2);
+            expect(calculateArea(3, 4)).toEqual(12);
+            expect(calculateArea(38, 2)).toEqual(76);
+        });
+
+        it("should return undefined if any of the arguments is not provided", () => {
+            expect(calculateArea(1)).toEqual(undefined);
+            expect(calculateArea()).toEqual(undefined);
+            expect(calculateArea(undefined, 1)).toEqual(undefined);
+        });
+
+        it("should return undefined if any of the arguments is not a number", () => {
+            expect(calculateArea('test', 1)).toEqual(undefined);
+            expect(calculateArea(-6, 'test')).toEqual(undefined);
+            expect(calculateArea('try', 'catch')).toEqual(undefined);
+            expect(calculateArea(0, true)).toEqual(undefined);
+            expect(calculateArea(false, 10)).toEqual(undefined);
+        });
+
+    });
+});
